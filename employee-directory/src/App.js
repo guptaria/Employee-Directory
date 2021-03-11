28d0f76860ee41d0e7c360a2ba5c001cd9d4370e
Priya Garg <gupta.ria100@gmail.com>
2021-03-11 00:46:34 -0600

    adding header and search component

---
+++ employee-directory/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header/index';
+import Search from './components/Search/index';
+
+function App() {
+  return (
+    <>
+<Header />
+<Search />
+< />
+ 
+  );
+}
+
+export default App;
