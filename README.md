c1a64c13c6ca29768bbcb5ea55db595f67f89564
Priya Garg <gupta.ria100@gmail.com>
2021-03-16 00:38:28 -0500

    adding readme

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # Employee-Directory
+
+### Description 
+This React-based app demonstrates an employee directory that can be searched and sorted. The directory is filled via an API call to Random User Generator and includes photo, name, age, e-mail and mobile phone number. Entering text into the search box will dynamically search  fields and filter those who match the entered text. The directory can be sorted by name or by age, as indicated by the change in cursor.
+
+
+## Table of Contents 
+* [Live](#Live)
+* [Acceptance_Criteria](#Acceptance_Criteria)
+* [Usage](#Usage)
+* [Installation](#Installation)
+* [Screenshots](#Screenshots)
+* [Github_Link](#Github_Link)
+
+
+## Live
+https://employee-directry.herokuapp.com/
+
+
+## Acceptance_Criteria
+As a user, 
+I want to be able to view my entire employee directory at once so that I have quick access to their information.
+
+## Usage
+Navigate to https://employee-directry.herokuapp.com/ <br>
+Search the employee directory by entering whatever search parameters you want into the Search box.<br>
+You can sort employees by clicking on Nmae buttons. Please note these buttons are toggles, and will adjust the sort direction with each click.
+
+
+## Installation 
+All NPM packages needed for this application are already listed as dependencies in the package.json file. If running this web application in your local environment, please make sure to run the npm install command in your terminal at the root directory level in order to access all required package.
+```
+npm install
+```
+
+
+
+## Screenshots
+
+![](src/assets/screenshot1.png)
+![](src/assets/screenshot2.png)
+
+
+
+
+## Github_Link
+[**URL of My Github Repository**](https://github.com/guptaria/Employee-Directory)<br>
+
+## Portfolio
+[**Portfolio**](https://guptaria.github.io/updated_portfolio/)
+
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -28,8 +28,8 @@ function Pages() {
         console.log(res.data.results);
         //   setEmployeeState({ ...employeeState, results: res.data.results });
         // },[])
-        { setEmployeeState(res.data.results) }
-        { setManipulateState(res.data.results) }
+        setEmployeeState(res.data.results)
+        setManipulateState(res.data.results)
 
       })
 
@@ -40,32 +40,58 @@ function Pages() {
   const handleInputChange = (event) => {
     const InputSearch = event.target.value.toLowerCase(); //Priya
     console.log(InputSearch);
-    setTempSearch(InputSearch);
-    const results = manipulateState.filter(mySearch => {
+    // setTempSearch(InputSearch);
+    const results = employeeState.filter(mySearch => {
       console.log(mySearch.name.first);
       return mySearch.name.first.toLowerCase().includes(event.target.value) || mySearch.name.last.toLowerCase().includes(event.target.value)
     });   //includes or indexof
     //sort array of objects 
     // console.log(results);
 
-    if (!InputSearch) {
-      setEmployeeState(results);
+    if (InputSearch) {
+      console.log("results");
+      setManipulateState(results);
+
     }
     else {
-      setManipulateState(results);
+      setManipulateState(employeeState);
     }
 
   };
 
-  const handleSortByName = event => {
-    console.log("handleSortByNmae");
+  // const handleSortByName = event => {
+  //   console.log("handleSortByNmae");
+  //   if (!sortState) {
+  //     setEmployeeState(employeeState.sort((a, b) => (a.name > b.name) ? 1 : -1));
+  //     setSortState(true);
+  // } else {
+  //   setEmployeeState(employeeState.sort((a, b) => (a.name > b.name) ? -1 : 1));
+  //     setSortState(false);
+  // }
+  // };
+
+  const handleSortByName = () => setSortState(!sortState);
+
+
+  const customSort = (a, b) => {
     if (!sortState) {
-      setEmployeeState(employeeState.sort((a, b) => (a.name > b.name) ? 1 : -1));
-      setSortState(true);
-  } else {
-    setEmployeeState(employeeState.sort((a, b) => (a.name > b.name) ? -1 : 1));
-      setSortState(false);
-  }
+      if (a.name.first > b.name.first) {
+        return 1
+      }
+      else {
+        return -1;
+      }
+
+    }
+    else if (sortState) {
+      if (a.name.first > b.name.first) {
+        return -1
+      }
+      else {
+        return 1;
+      }
+    }
+    return 0;
   };
 
   const handleSortByAge = event => {
@@ -101,21 +127,21 @@ function Pages() {
       <Header>
         <h1 className="title text-5xl text-gray-800 mt-16">Employee Directory</h1>
         <p className="mb-16 text-md">Search for an employee or sort by Name or Category.</p>
-        </Header>
+      </Header>
 
       <Search handleInputPlaceHolder={handleInputChange}
         handleSortByName={handleSortByName}
         handleSortByAge={handleSortByAge}
       />
 
-      {manipulateState.map(person => {
+      {manipulateState.sort(customSort).map(person => {
         return <EmployeeCard
           key={person.email}
           image={person.picture.large}
           Name={person.name.first}
           Country={person.location.country}
           Email={person.email}
-          Age ={person.dob.age}
+          Age={person.dob.age}
           Contact={person.phone}
         />
       })}
